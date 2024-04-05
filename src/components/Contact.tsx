b2feba5ac36de703ce92e21584d8655fa01bd92b
Garri Shtyrkin <garrishtyrkin@gmail.com>
2024-04-05 18:15:31 -0400

    change contact form 5.0

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,97 +1,58 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import { Element } from "react-scroll";
-// import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const Contact: React.FC = () => {
 
 
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-  // const reset = () => {
-  //   setName('');
-  //   setEmail('');
-  //   setMessage('');
-  // }
+  const reset = () => {
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
 
-  // const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-  //   const form = event.target as HTMLFormElement;
-  //   const formData = new FormData(form);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
 
-  //   if (name && email && message) { 
-  //       try {
-  //           const formDataParams = new URLSearchParams();
-  //           formData.forEach((value, key) => {
-  //               formDataParams.append(key, value.toString());
-  //           });
+    if (name && email && message) { 
+        try {
+            const formDataParams = new URLSearchParams();
+            formData.forEach((value, key) => {
+                formDataParams.append(key, value.toString());
+            });
   
-  //           const response = await fetch('/', {
-  //             method: 'POST',
-  //             body: formDataParams,
-  //             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //           });
+            const response = await fetch('/', {
+              method: 'POST',
+              body: formDataParams,
+              headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            });
       
-  //           if (response.ok) {
-  //             toast('Success! Your message has been sent.');
-  //             reset();
-  //           } else {
-  //             toast('Error sending message. Please try again.');
-  //           }
-  //         } catch (error) {
-  //           toast('Error sending message. Please try again.');
-  //         }
-  //   } else {
-  //       toast('Please fill out all fields.');
-  //   }
-  // };
-
-
-  // const [name, setName] = useState<string>("");
-  // const [email, setEmail] = useState<string>("");
-  // const [message, setMessage] = useState<string>("");
-
-  // const encode = (data: { [key: string]: string }): string => {
-  //   return Object.keys(data)
-  //     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-  //     .join("&");
-  // }
-
-  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
-  //   e.preventDefault();
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({ "form-name": "contact", name, email, message })
-  //   })
-  //     .then(() => alert("Success!"))
-  //     .catch(e => toast(e));
-  // };
-
-  // const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
-  //   const { name, value } = e.target;
-  //   switch (name) {
-  //     case 'name':
-  //       setName(value);
-  //       break;
-  //     case 'email':
-  //       setEmail(value);
-  //       break;
-  //     case 'message':
-  //       setMessage(value);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // };
+            if (response.ok) {
+              toast('Success! Your message has been sent.');
+              reset();
+            } else {
+              toast('Error sending message. Please try again.');
+            }
+          } catch (error) {
+            toast('Error sending message. Please try again.');
+          }
+    } else {
+        toast('Please fill out all fields.');
+    }
+  };
 
   return (
     <Element name="contactSection" className="element">
       <div className="contact" id="contact">
         <form
-          // onSubmit={handleSubmit}
+          onSubmit={handleSubmit}
           className="contact-form"
           name="contactForm"
           data-netlify="true"
@@ -106,33 +67,33 @@ const Contact: React.FC = () => {
 
           <div className="contact-info">
             <input
-              // value={name}
+              value={name}
               name="name"
               placeholder="Full Name"
               type="text"
-              // onChange={handleChange}
+              onChange={(e) => setName(e.target.value)}
             />
             <input
-              // value={email}
+              value={email}
               name="email"
               placeholder="Email Address"
               type="email"
-              // onChange={handleChange}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
           <textarea
-            // value={message}
+            value={message}
             name="message"
             placeholder="Message description"
-            // onChange={handleChange}
+            onChange={(e) => setMessage(e.target.value)}
           ></textarea>
 
           <div>
             <button type="submit">Send</button>
           </div>
         </form>
-        {/* <ToastContainer /> */}
+        <ToastContainer />
       </div>
     </Element>
   );
